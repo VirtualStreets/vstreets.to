a2a16a6ff437b7d745acfb0ec1811a5c9d4c95bc
sebm253 <42180891+sebm253@users.noreply.github.com>
2025-06-23 14:40:12 +0200

    fix: redirect on client side

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-const redirect = (url) => new Response(null, { status: 301, headers: { 'Location': url } });
-const website_redirect = redirect('https://virtualstreets.org');
+const websiteUrl = 'https://virtualstreets.org';
+const redirect = (url) => new Response(`
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="refresh" content="0;url=${url}">
+  <script>location.replace("${url}");</script>
+</head>
+<body style="margin:0;padding:0;background:#fff;"></body>
+</html>
+`, {
+  headers: { 'Content-Type': 'text/html' },
+  status: 200,
+});
 
 export default {
   async fetch(request, env, ctx) {
     const { pathname } = new URL(request.url);
-    const slug = pathname.substring(1);
+    const slug = pathname.substring(1).toLowerCase();
+
     if (!slug) {
-      return website_redirect;
+      return redirect(websiteUrl);
     }
     try {
-      const value = await env.vstreets.get(slug.toLowerCase());
-      return value ? redirect(value) : website_redirect;
+      const value = await env.vstreets.get(slug);
+      return value ? redirect(value) : redirect(websiteUrl);
     } catch (e) {
       return new Response(e.message, { status: 500 });
     }
